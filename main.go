62f0914ce0a33477fb8e10173d6a0097368eff38
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-10-25 11:29:36 +0200

    Initial action

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var prNumber int
+	flag.IntVar(&prNumber, "pr-number", 0, "The number of the Agent Pool controller workers.")
+	flag.Parse()
+
+	if prNumber == 0 {
+		fmt.Println("PR number is not passed")
+		os.Exit(1)
+	}
+
+	fmt.Println("PR number:", prNumber)
+
+	// r, ok := os.LookupEnv("GITHUB_REPOSITORY")
+	// if !ok {
+	// 	fmt.Println("Failed to get the repository via GITHUB_REPOSITORY")
+	// 	os.Exit(1)
+	// }
+	// rr := strings.Split(r, "/")
+
+	// client := github.NewClient(nil)
+	// files, _, err := client.PullRequests.ListFiles(context.TODO(), rr[0], rr[1], prNumber, &github.ListOptions{})
+	// if err != nil {
+	// 	fmt.Println(fmt.Errorf("Failed to get a file list: %s", err.Error()))
+	// 	os.Exit(1)
+	// }
+
+	// for _, f := range files {
+	// 	fmt.Println(*f.Filename)
+	// }
+}
