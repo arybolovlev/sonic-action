2c63cbfc7023d9db1f086ef66cc28b0a20b1874c
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-10-25 11:50:01 +0200

    Action

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/arybolovlev/sonic-action
 
 go 1.21.3
 
-require github.com/google/go-github/v56 v56.0.0
+require github.com/google/go-github v17.0.0+incompatible
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
 )
 
 func main() {
@@ -18,21 +22,21 @@ func main() {
 
 	fmt.Println("PR number:", prNumber)
 
-	// r, ok := os.LookupEnv("GITHUB_REPOSITORY")
-	// if !ok {
-	// 	fmt.Println("Failed to get the repository via GITHUB_REPOSITORY")
-	// 	os.Exit(1)
-	// }
-	// rr := strings.Split(r, "/")
+	r, ok := os.LookupEnv("GITHUB_REPOSITORY")
+	if !ok {
+		fmt.Println("Failed to get the repository via GITHUB_REPOSITORY")
+		os.Exit(1)
+	}
+	rr := strings.Split(r, "/")
 
-	// client := github.NewClient(nil)
-	// files, _, err := client.PullRequests.ListFiles(context.TODO(), rr[0], rr[1], prNumber, &github.ListOptions{})
-	// if err != nil {
-	// 	fmt.Println(fmt.Errorf("Failed to get a file list: %s", err.Error()))
-	// 	os.Exit(1)
-	// }
+	client := github.NewClient(nil)
+	files, _, err := client.PullRequests.ListFiles(context.TODO(), rr[0], rr[1], prNumber, &github.ListOptions{})
+	if err != nil {
+		fmt.Println(fmt.Errorf("Failed to get a file list: %s", err.Error()))
+		os.Exit(1)
+	}
 
-	// for _, f := range files {
-	// 	fmt.Println(*f.Filename)
-	// }
+	for _, f := range files {
+		fmt.Println(*f.Filename)
+	}
 }
